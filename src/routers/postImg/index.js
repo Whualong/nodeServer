d71b0fc332b8cb9328f2b6b6b26a14cfe0104702
张子豪 <1844023095@qq.com>
2022-01-03 00:50:33 +0800

    init

---
+++ src/routers/postImg/index.js
@@ -0,0 +1,13 @@
+module.exports = {
+  path: "/api/post_img",
+  async handle(ctx, next) {
+    // 拿到请求输入
+    let params = ctx.request.body;
+    
+    // 业务逻辑处理
+    console.log(params);
+
+    // 返回前端数据
+    ctx.body = { res: "SUCCESS", data: 'ok', code: 2000 };
+  }
+};
---
+++ src/routers/routers.js
@@ -0,0 +1,6 @@
+let demo = require('./demo/index.js')
+let img = require('./postImg/index.js')
+module.exports = [
+  demo,
+  img
+]
